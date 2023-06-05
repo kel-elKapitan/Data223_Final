e5f117bdb69923473ef03740aa6bb462bcf2a90b
Kelly McGowan <kelly.c.mcgowan80@gmail.com>
2023-06-05 20:11:12 +0100

    Create ETL.py
    Created a structure to the file for functions to be added later

---
+++ ETL.py
@@ -0,0 +1,23 @@
+# Functions to Extract, transform (many files into one) and load data back into s3 bucket under a different name
+
+
+# JSON files into one file
+
+
+
+# CSV files into one file
+
+
+
+# Create CSV from JSON
+
+
+
+
+# Create JSON from CSV
+
+
+
+
+
+
