875f02452f9c9196a603cd48df4517708c2cc5fc
kel-elkapitan <mr.kelly.mcgowan@gmail.com>
2023-06-06 01:14:41 +0100

    created many2one_json() function

--- ETL.py
+++ ETL.py
@@ -3,6 +3,30 @@
 
 # JSON files into one file
 
+def many2one_json():
+    import boto3
+    import json
+
+    big_one = []
+
+    s3 = boto3.resource('s3')   
+
+    bucket = s3.Bucket('s3://data-eng-223-final-project/')
+
+     # loop over all files in s3 folder
+    for obj in bucket.objects.filter(Prefix='Talent/'):
+    
+    # check to see if the file is a json file
+        if obj.key.endswith('.json'):
+            key = obj.key
+            body = obj.get()['Body'].read()
+            json_data = json.loads(body)
+            big_one.append(json_data)
+            
+
+    # send to s3 bucket
+    s3 = boto3.client('s3')
+    s3.put_object(Bucket = 'data-eng-223-final-project', Key='ONE_BIG_JSON.json', Body=json.dumps(big_one))
 
 
 # CSV files into one file
